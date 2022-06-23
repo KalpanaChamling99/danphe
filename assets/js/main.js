a68536e1a88156bad0fdbcc6b7f487c5470b5f6d
kalpanachamling99 <kalpanachamling99@gmail.com>
2022-06-23 13:18:48 +0545

    remove console log

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ function animation() {
   const animations = document.querySelectorAll('[data-animation');
   animations.forEach(animation => {
     const animationStatus = animation.style.animationPlayState;
-    console.log(animationStatus);
     if( animationStatus === 'running'){
       animation.style.animationPlayState = 'paused';
     }else{
